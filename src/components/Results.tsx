d5a98d2fb12414a49a101a5e67efcb1c2b723ff4
Drew Alexander <drewalexander986@gmail.com>
2023-07-17 00:08:19 +0800

    better end screen

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,16 +1,22 @@
 import { useContext } from "react"
 import { GameContext } from "../GameContext"
 import { ThemeContext } from "../ThemeContext"
+import { colourToHex } from "../utils/colourUtils"
 
 export function Results() {
-    const { guessList } = useContext(GameContext)
-    const darkTheme = useContext(ThemeContext)
+    const { guessList, goal } = useContext(GameContext)
     const lastGuess = guessList.at(-1)
-    const colour = lastGuess ? lastGuess : darkTheme ? '#252525' : '#FFF'
 
     return (
-        <div style={{ color: colour }} className='text-6xl font-extrabold'>
-                You Win
+        <div className="text-white dark:text-black flex w-64 h-64 border-4">
+            <div style={{ backgroundColor: lastGuess }} className='flex flex-col justify-end items-center h-full w-1/2'>
+                <span className="text-xs">Your guess</span>
+                <span className='font-extrabold'>{ lastGuess?.toUpperCase() }</span>
+            </div>
+            <div style={{ backgroundColor: colourToHex(goal) }} className="flex flex-col justify-end items-center h-full w-1/2">
+                <span className="text-xs">Actual hex</span>
+                <span className='font-extrabold'>{ colourToHex(goal).toUpperCase() }</span>
+            </div>
         </div>
     )
 }
